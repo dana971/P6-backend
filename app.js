1b19f287cb62692a40a78f9c316fa624a8febd2f
dana971 <nac.dara@gmail.com>
2023-01-31 13:28:28 +0100

    app.js
    Amélioration du cors +ajout hemlet

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
-
+const path = require('path');
 
 //Les routes
 const userRoutes = require('./routers/user');
 const sauceRoutes = require ('./routers/sauces');
+
 
 app.use(express.json());
 
@@ -17,17 +21,27 @@ mongoose.connect('mongodb+srv://dara:WzaGORLcrEujseN@cluster0.ace8fne.mongodb.ne
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE , PATCH, OPTIONS');
-    next();
-});
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+})
+
+app.use(bodyParser.json());
+
+
+app.use(helmet());
 
 //appel des routes
 
+app.use('/images', express.static(path.join(__dirname,'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
 
 
 module.exports = app;
--- routers/user.js
+++ routers/user.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const userAuth = require('../middleware/auth');
+//const userAuth = require('../middleware/auth');
 
 const userController = require('../controllers/user');
 
 
 
 router.post('/signup',userController.signup);
-router.post('/login',userAuth,userController.login);
+router.post('/login',userController.login);
 
 module.exports = router;
