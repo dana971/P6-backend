742edf02c3b304dc12e7aa187c5f9b2390255fb4
dana971 <nac.dara@gmail.com>
2023-01-31 13:25:56 +0100

    model User
    Suite erreur MD maj modèle user

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const validator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema ({
-    email : {type: String, required: true, unique: true},
+    email : {type: String, required: true },
     password: {type: String, required: true }
 });
 
-//userSchema.plugin(validator);
+userSchema.plugin(validator);
 module.exports = mongoose.model('user', userSchema);
